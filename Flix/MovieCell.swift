291a8d4a541be3dd6d4cc08a6a000c35f2c7a179
Adnaan Syed <adnaan.syed01@gmail.com>
2022-09-17 12:57:28 -0400

    Fixed Table View and created cells illustrating title & summary of each move

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Adnaan_Syed on 9/17/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -7,17 +7,9 @@
 
 import UIKit
 
+//These classes allow a View Conroller to interact with a UITableView
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        
-    }
-    
-    
-    
+
     //MARK: Have to check out (movies is an array of dictionaries???)
     var movies = [[String:Any]]()
 
@@ -26,12 +18,15 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //MARK: The functions for TableView would not be called if these assignments were not made.
+        tableView.dataSource = self
+        tableView.delegate = self
 
-        print("hello")
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
         /*
-         MARK: Ignoring this part FOR NOW
+         MARK: Ignoring this part FOR NOW; Downloads a list of movies, along with other info, and then stores the list into an array of dics
          */
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -46,6 +41,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                  
                  
                  self.movies = dataDictionary["results"] as! [[String:Any]]
+                 
+                 //MARK: need to reload the tableview (the below functions) in order to display the imported data
+                 self.tableView.reloadData()
 
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
@@ -58,6 +56,35 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
     }
     
+    /* MARK: These functions are necessary for the interaction of TableView and View Controllers, wtf is indexPath?????
+     MARK: These functions are only loaded thus far when view is loaded (need to reload table view after data is downloaded)
+     */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return movies.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //MARK: Now want to recycle the cells that were created before (but are no longer shown b/c of screen size) instead of creating hundreds of cells for each new movie. that's why we use reusable cells!!!
+        //Also want to connect to our custom MovieCell class.
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell //(again unwrapping)
+        
+        let movie = movies[indexPath.row]
+        
+        let title = movie["title"] as! String
+        
+        let sum = movie["overview"] as! String
+        
+
+        //MARK: Another example of optional checking with either ! or ?
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = sum
+        return cell;
+    }
+    
+
 
     /*
     // MARK: - Navigation
